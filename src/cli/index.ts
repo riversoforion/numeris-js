74e9da4798095a82a8f67d44b703c159f1fb45ef
Eric McIntyre <mac@riversoforion.com>
2020-04-13 12:42:26 -0700

    Implemented CLI functionality
    Converted values based on the command flags. Added flags for debug &
    bare output. Colorized default output.
    
    Issue #4

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,19 +1,48 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
+import { integerToRoman, romanToInteger } from '../index';
+
 const { Command } = require('commander');
+
+const VERSION = process.env.npm_package_version;
+const EXIT_BAD_ARGS = 1;
+const EXIT_BAD_INPUT = 2;
 
 const program = new Command();
 program
-  .version('0.0.1')
+  .version(VERSION)
   .description('Romanus - a roman numeral converter')
-  .option('-i, --integer', 'Convert the given integer value to a roman numeral')
-  .option('-r, --roman', 'Convert the given roman numeral to an integer value');
+  .option('-i, --integer <number>', 'Convert the given integer value to a roman numeral', parseInt)
+  .option('-r, --roman <numeral>', 'Convert the given roman numeral to an integer value')
+  .option('-d, --debug', 'Debugging output')
+  .option('-b, --bare', 'Only output the result');
 
 program.parse(process.argv);
 
+const color = new chalk.Instance({
+  level: program.bare ? 0 : chalk.level,
+});
+
 if (!program.integer && !program.roman) {
-  console.error('Must specify one of --integer or --roman');
-  process.exit(1);
+  console.error(color`{red {bold.inverse ERROR:} Must specify one of --integer or --roman}`);
+  process.exit(EXIT_BAD_ARGS);
 }
-// If --integer, call integerToRoman
-// If --roman, call romanToInteger
-// Output the result
+
+if (program.debug) {
+  console.debug(color`{dim integer =} ${program.integer}`);
+  console.debug(color`{dim roman   =} ${program.roman}`);
+}
+
+try {
+  const result: string | number = program.integer
+    ? integerToRoman(program.integer)
+    : romanToInteger(program.roman);
+
+  const RESULT_PREFIX = program.bare ? '' : color`{bold.inverse RESULT:} `;
+  console.log(color`{green ${RESULT_PREFIX}${result}}`);
+}
+catch (error) {
+  const ERROR_PREFIX = program.bare ? '' : color`{bold.inverse ERROR:} `;
+  console.error(color`{red ${ERROR_PREFIX}${error.message}}`);
+  process.exit(EXIT_BAD_INPUT);
+}
